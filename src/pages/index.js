74b8953b29bdab31b3e07088d0fc6b1ae5cba0f4
joporicci <juanricci488@gmail.com>
2024-12-01 18:48:30 -0300

    agrego denominación al index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer"
 import LoginForm from "@/components/loginform"
 
-export default function(){
+export default function HomePage(){
   return (
     <div>
       <LoginForm/>
