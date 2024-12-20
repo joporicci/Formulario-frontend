6ae50db676a120c61dc4e4dd7f05593e8ad89f1c
Franco Fuschino <francofuschino@gmail.com>
2024-12-20 08:20:30 -0300

    fix para la subida de imagenes

--- api/axios.js
+++ api/axios.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // URL base del backend
   withCredentials: true, // Habilitar el envío de cookies para autenticación
-  headers: {
-    'Content-Type': 'application/json', // Asegúrate de que el encabezado sea correcto
-  },
+  // headers: {
+  //   'Content-Type': 'application/json', // Asegúrate de que el encabezado sea correcto
+  // },
 });
 
 export default axiosInstance;
